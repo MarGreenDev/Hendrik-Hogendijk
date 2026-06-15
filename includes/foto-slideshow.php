c43e7d40097617e68958fe80dba069c9e0a696d8
MarGreenDev <marylougroeneveld@gmail.com>
2026-06-15 09:59:29 +0200

    add working image slideshow

---
+++ includes/foto-slideshow.php
@@ -0,0 +1,5 @@
+<div class="foto-slideshow-container">
+    <img src="assets/images/foto-tuin-1.webp" alt="tuin foto 1" class="mySlides">
+    <img src="assets/images/foto-tuin-2.jpeg" alt="tuin foto 2" class="mySlides">
+    <img src="assets/images/foto-tuin-3.webp" alt="tuin foto 3" class="mySlides">
+</div>
--- index.php
+++ index.php
@@ -2,5 +2,13 @@
 include 'includes/header.php'
 ?>
 
+<!-- slideshow -->
+<?php include 'includes/foto-slideshow.php'
+?>
+<!-- diensten -->
+
+<!-- reviews -->`
+     <script src="assets/js/script.js"></script>
+
 <?php 
 include 'includes/footer.php' ?>
