8c27bbe830979b5bc7b251a37089578c2102bf12
MarGreenDev <marylougroeneveld@gmail.com>
2026-06-15 08:57:17 +0200

    extract header and footer into includes

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <img src="./assets/images/logo.webp" alt="logo">
+        <h1>Hendrik Hogendijk Hoveniers</h1>
+        <div class="phone_section">
+            <img src="./assets/images/phone_icon.png" alt="phone">
+            <p class="phone_number">tel-06123465</p>
+        </div>
+    </header>
