b4ecffcae375ac8496ac4973742f09625d98002b
MarGreenDev <marylougroeneveld@gmail.com>
2026-06-15 11:26:28 +0200

    voeg diesnten toe aan homepage

---
+++ includes/diensten.php
@@ -0,0 +1,30 @@
+<div id="diensten-container">
+    <div class="dienst">
+        <div>
+        <img src="assets/images/icon-tuinaanleg.png" alt="icoon tuinaanleg">
+        </div>
+        <h3>Tuinaanleg</h3>
+        <p>Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken. </p>
+    </div>
+    <div class="dienst">
+        <div>
+        <img src="assets/images/icon-tuinonderhoud.png" alt="icoon tuinonderhoud">
+        </div>
+        <h3>Tuinonderhoud</h3>
+        <p>Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft.</p>
+    </div>
+    <div class="dienst">
+        <div>
+        <img src="assets/images/icon-tuinontwerp.png" alt="icoon tuinontwerp">
+        </div>
+        <h3>tuinontwerp</h3>
+        <p>Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen aankomt, maakt Hendrik Hogendijk het verschil.</p>
+    </div>
+    <div class="dienst">
+        <div>
+        <img src="assets/images/icon-zwemvijvers.png" alt="icoon zwemvijvers">
+        </div>
+        <h3>zwemvijvers</h3>
+        <p>Zwemvijvers, is één van mijn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd. </p>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -3,12 +3,11 @@ include 'includes/header.php'
 ?>
 
 <!-- slideshow -->
-<?php include 'includes/foto-slideshow.php'
-?>
+<?php include 'includes/foto-slideshow.php' ?>
 <!-- diensten -->
+<?php include 'includes/diensten.php' ?>
+<!-- reviews -->
 
-<!-- reviews -->`
-     <script src="assets/js/script.js"></script>
-
-<?php 
+<script src="assets/js/script.js"></script>
+<?php
 include 'includes/footer.php' ?>
