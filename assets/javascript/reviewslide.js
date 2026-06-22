599681338ec7597ef355983c322ed5d5d535eb52
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-06-22 09:16:32 +0200

    fix reviews css issue

--- assets/javascript/reviewslide.js
+++ assets/javascript/reviewslide.js
@@ -1,23 +1,25 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let sIndex = 1;
+showSlides(sIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(sIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(sIndex = n);
 }
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("review_slide");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { sIndex = 1 }
+    if (n < 1) { sIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
+            console.log(slides);
+
         slides[i].classList.remove("active");
     }
-    slides[slideIndex - 1].classList.add("active");
+    slides[sIndex - 1].classList.add("active");
 }
