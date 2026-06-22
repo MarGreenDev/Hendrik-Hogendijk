9513c7c81198875efcd7227c16c0cc0a788e4a46
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-06-22 13:26:12 +0200

    added a fade animation to the reviews

--- includes/reviews.php
+++ includes/reviews.php
@@ -9,7 +9,7 @@ $result = $conn->query($sql);
 <div class="review_container">
 
     <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)) { ?>
-        <div class="review_slide">
+        <div class="review_slide fade">
             <div class="review_card">
                 <div class="username"><?= $row['naam'] ?></div>
                 <p class="review_text"><?= $row['bericht'] ?></p>
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 
     <?php } ?>
 
-        <!-- prev and next buttons -->
+    <!-- prev and next buttons -->
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
 
