f0182178c85df280d874f6c916f6311b4f667b5e
my-st4rfire <sn.vdbilt@gmail.com>
2026-06-25 10:14:10 +0200

    login pagina klaar

--- admin/login_register.php
+++ admin/login_register.php
@@ -11,6 +11,8 @@ if (isset($_POST['login'])) {
     $stmt->execute(['email' => $email]);
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    // $hash = password_hash('hovenier', PASSWORD_DEFAULT);
+
 
     if ($user) {
         if (password_verify($password, $user['password'])) {
